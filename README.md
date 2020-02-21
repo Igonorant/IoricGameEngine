7e8dbac6edbf7dc92673b904543813df7c5703f5
igolino3@hotmail.com <igorsmarques0@gmail.com>
2020-02-20 22:04:22 -0300

    SDL and SDL image working!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ioric Game Engine
+
+First from scratch game engine! :)
+
+Documentation: [Google Docs Link](https://docs.google.com/document/d/1O6a_ADm4j3djTGG7ja0TbP9R5Zgx-lRtOZkJ7Wawj18/edit?usp=sharing)
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,7 +1,28 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
 
-
-
-void main()
+int main(int argc, char* argv[])
 {
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
 
+	// Load support for the JPG and PNG image formats
+	int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+	int initted = IMG_Init(flags);
+	if ((initted & flags) != flags) {
+		SDL_Log("IMG_Init: Failed to init required jpg and png support!\n");
+		SDL_Log("IMG_Init: %s\n", IMG_GetError());
+		SDL_Quit();
+		return 2;
+	}
+
+	SDL_Log("Initialization of SDL and SDL image successfully!");
+
+	std::cin.get();
+	SDL_Quit();
+	return 0;
 }
